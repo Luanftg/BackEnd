6016740c8c051981602eb4942c7f82df094a72f3
Luanftg <luanftgimenez@gmail.com>
2022-07-24 19:30:59 -0300

    conecão com o banco de daos e atualizando README

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import express, { json } from 'express';
 import routes from './routes/index.js';
+import db from './database/index.js';
 
 const PORT =  process.env.PORT || 3000;
 const app = express();
+
+db.hasConection();
+
 app.use(express.json());
 app.use(routes);
 
