20e30d3519a6f2d56c54971e0c19709e5b5722c8
Luanftg <luanftgimenez@gmail.com>
2022-07-25 11:27:08 -0300

    adicionando moddlewares bloqueio e requestLog

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import filmeController from '../controllers/filmesController.js';
 import healthCheck from '../controllers/healthCheck.js';
+import bloqueio from '../middlewares/bloqueio.js';
+import requestLog from '../middlewares/requestLog.js';
+
 const routes = express.Router();
 
 routes.get('/', healthCheck.health);
 
-routes.get('/filmes', filmeController.listarFilme);
+routes.get('/filmes', requestLog, bloqueio ,filmeController.listarFilme);
 routes.get('/filmes/:id', filmeController.buscaFilmeID);
 routes.post('/filmes', filmeController.cadastrarFilme);
 routes.delete('/filmes/:id', filmeController.deletarFilme);
